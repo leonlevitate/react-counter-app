4f68d5ed91c395d8f5c8b4651193b0a23ca4091e
Leon Linton <leon.linton@live.co.uk>
2019-08-06 22:35:37 +0100

    Refactored buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,15 +6,18 @@ class Counter extends Component {
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <React.Fragment>
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
