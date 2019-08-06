8734f9b978b8d91a43991d24cfa70a19c99d5336
Leon Linton <leon.linton@live.co.uk>
2019-08-06 22:31:15 +0100

    Dynamic buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,10 +6,13 @@ class Counter extends Component {
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
