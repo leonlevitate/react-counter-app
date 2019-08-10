00dbb5ff456f8429202f6b513c0f377c15757dd4
Leon Linton <leon.linton@live.co.uk>
2019-08-10 20:33:47 +0100

    Bug fix for passing event arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,17 +11,21 @@ class Counter extends Component {
     this.handleIncrement = this.handleIncrement.bind(this);
   }
 
-  handleIncrement(product) {
+  handleIncrement = product => {
     console.log(product);
     this.setState({ count: this.state.count + 1 });
-  }
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={this.doHandleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
